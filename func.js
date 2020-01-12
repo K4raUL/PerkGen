79cda4fdb9c8125264f69274051e3981f2671e22
K4raUL <k4raul@gmail.com>
2020-01-12 15:07:48 +0300

    Update func.js

--- func.js
+++ func.js
@@ -34,7 +34,6 @@ function getCookie(cname) {
 function init() {
     // reading statistics from cookies
     stats = getCookie("stats")
-    alert(document.cookie)
     if (!stats) stats = []
     
     //perks = getCookie("perks")
