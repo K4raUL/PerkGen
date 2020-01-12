aba73ae4e755bc6a6a66e23e491c7de95293337d
K4raUL <k4raul@gmail.com>
2020-01-12 15:13:27 +0300

    Update func.js

--- func.js
+++ func.js
@@ -83,7 +83,6 @@ function generate() {
         else stats[rid]++
         
         // displaying statistics
-	alert(stats[rid])
         document.getElementById("res" + iter).innerHTML = stats[rid]
         
         // erasing perk from list (preventing same perk appearing in the result)
